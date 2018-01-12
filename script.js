c4da66e7d3d1ca59e7806cd9cde455e87a750aef
Nathan Baker <nathanmbaker@gmail.com>
2018-01-12 12:17:40 -0800

    Icons Added. Remove Feature Done.
    I added icons for search, edit, remove and add. Additionally, I am
    removing objects well.

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var students = [];
 var sections = [];
 function createHTMLElements() {
     selectBox = document.getElementById("additionSelectBox");
+    newObjectDiv = document.getElementById("addForm");
+    removeObjectDiv = document.getElementById("removeDiv");
     newStudentForm = document.getElementById("newStudentForm");
     newTeacherForm= document.getElementById("newTeacherForm");
     newSectionForm = document.getElementById("newSectionForm");
@@ -19,6 +21,7 @@ function createHTMLElements() {
     newSectionSelectTeacherBox = document.getElementById("newSectionSelectTeacher");
     removeSelectBox = document.getElementById("removeSelectBox");
     objectRemovalSelectBox = document.getElementById("objectForRemovalSelectBox");
+    removeButton = document.getElementById("removeButton");
 }
 function Person(firstName,lastName) {
     this.id = id;
@@ -68,6 +71,28 @@ function Section(name, maxSize, teacher) {
 
 
 }
+function iconClicked(button) {
+    messageDiv.innerHTML = '';
+    switch (button) {
+        case "add":
+            newObjectDiv.style.display = "inline";
+            removeObjectDiv.style.display = "none";
+            break;
+        case "remove":
+            newObjectDiv.style.display = '';
+            if (students.length == 0 && teachers.length == 0 && sections.length == 0) {
+                removeObjectDiv.style.display = "none";
+                messageDiv.innerHTML = "You have nothing to remove";
+            } else {
+                removeObjectDiv.style.display = "inline";
+            }
+            break;
+        case "search":
+            break;
+        case "edit":
+            break;
+    }
+}
 
 function selectBoxOptionChosen() {
     messageDiv.innerHTML = "";
@@ -109,7 +134,7 @@ function addObject() {
     var newObject;
     switch (parseInt(selectBox.value)){
         case 0:
-            newObject = new Student(studentFirstNameTextBox.value, studentLastNameTextBox.value, parseInt(studentGradeSelectBox.value));
+            newObject = new Student(parseInt(studentGradeSelectBox.value), studentFirstNameTextBox.value, studentLastNameTextBox.value);
             studentFirstNameTextBox.value = "";
             studentLastNameTextBox.value = "";
             students.push(newObject);
@@ -119,7 +144,7 @@ function addObject() {
             newObject = new Teacher(teacherSubjectTextBox.value ,teacherFirstNameTextBox.value, teacherLastNameTextBox.value);
             teacherSubjectTextBox.value = "";
             teacherLastNameTextBox.value = "";
-            teacherFirstNameTextBox = "";
+            teacherFirstNameTextBox.value = "";
             teachers.push(newObject);
             messageDiv.innerHTML = "New Teacher Added!";
             break;
@@ -138,42 +163,66 @@ function removalBoxOptionChosen() {
     objectRemovalSelectBox.options.length = 0;
     switch (parseInt(removeSelectBox.value)) {
         case 0:
+            if (students.length == 0) {
+                messageDiv.innerHTML = "You have no students to remove";
+                removeButton.disabled= true;
+            } else {
+                removeButton.disabled = false;
+            }
             for (var i = 0; i < students.length; i++) {
                 var newEl = document.createElement("option");
                 newEl.value = i;
                 newEl.innerHTML = students[i].firstName + " " + students[i].lastName;
                 objectRemovalSelectBox.appendChild(newEl);
             }
+            break;
         case 1:
+            if (teachers.length == 0) {
+                messageDiv.innerHTML = "You have no teachers to remove";
+                removeButton.disabled = true;
+            } else {
+                removeButton.disabled = false;
+            }
             for (var i = 0; i < teachers.length; i++) {
                 var newEl = document.createElement("option");
                 newEl.value = i;
                 newEl.innerHTML = teachers[i].firstName + " " + teachers[i].lastName;
                 objectRemovalSelectBox.appendChild(newEl);
             }
+            break;
         case 2:
+            if (sections.length == 0) {
+                messageDiv.innerHTML = "You have no sections to remove";
+                removeButton.disabled = true;
+            } else {
+                removeButton.disabled = false;
+            }
             for (var i = 0; i < sections.length; i++) {
                 var newEl = document.createElement("option");
                 newEl.value = i;
                 newEl.innerHTML = sections[i].name;
                 objectRemovalSelectBox.appendChild(newEl);
             }
+            break;
     }
 }
 
 function removeObject() {
     switch (parseInt(removeSelectBox.value)){
         case 0:
-            students.splice(parseInt(objectRemovalSelectBox.value));
+            students.splice(parseInt(objectRemovalSelectBox.value), 1);
             messageDiv.innerHTML = "Student Removed!";
             removalBoxOptionChosen();
+            break;
         case 1:
-            teachers.splice(parseInt(objectRemovalSelectBox.value));
-            messageDiv.innerHTML = "Section Removed!";
-            removalBoxOptionChosen();
-        case 2:
-            sections.splice(parseInt(objectRemovalSelectBox.value));
+            teachers.splice(parseInt(objectRemovalSelectBox.value), 1);
             messageDiv.innerHTML = "Teacher Removed!";
             removalBoxOptionChosen();
+            break;
+        case 2:
+            sections.splice(parseInt(objectRemovalSelectBox.value), 1);
+            messageDiv.innerHTML = "Section  Removed!";
+            removalBoxOptionChosen();
+            break;
     }
 }
